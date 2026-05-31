96af778ad80631e9e3cd1a23ac9af22970089edf
Krishna Shah <shahkrishna2706@gmail.com>
2026-05-31 14:35:18 +0530

    Update main.py

--- app/dashboard/main.py
+++ app/dashboard/main.py
@@ -1,11 +1,19 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 
 st.title("MHA GestureNet")
 
+RTC_CONFIGURATION = RTCConfiguration({
+    "iceServers": [
+        {"urls": ["stun:stun.l.google.com:19302"]},
+        {"urls": ["stun:stun1.l.google.com:19302"]},
+    ]
+})
+
 webrtc_streamer(
     key="test",
-    media_stream_constraints={"video": True, "audio": False}
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False},
 )
 
 st.success("WebRTC Widget Loaded")
